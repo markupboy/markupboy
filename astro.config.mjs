2d39c7e745310f2c93cd62d322516aba2b76bc4b
Blake Walters <blake@markupboy.com>
2026-03-24 09:42:13 -0600

    removed dotfiles.sh redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,4 @@ export default defineConfig({
   build: {
     format: "directory",
   },
-  redirects: {
-    "/dotfiles.sh": {
-      status: 301,
-      destination: "https://raw.githubusercontent.com/markupboy/dotfiles/main/script/remote.sh",
-    },
-  },
 });
